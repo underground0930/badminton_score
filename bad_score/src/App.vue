8fd8fff0a933e474bfc34b329705b96dceb9fc82
go.nishiduka <go.nishiduka.1985@gmail.com>
2019-01-22 17:49:12 +0900

    シェア用snsを追加

--- bad_score/src/App.vue
+++ bad_score/src/App.vue
@@ -9,6 +9,7 @@
         <ul class="footer__links">
           <li>developer : <a href='https://twitter.com/resistance_gowy' target="_blank">@resistance_gowy</a></li>
           <li>github : <a href='https://github.com/underground0930/badminton_score' target="_blank">underground0930</a></li>
+          <li><a :href="twitterLink" target="_blank">Share on Twitter</a> | <a :href="facebookLink" target="_blank">Share on Facebook</a></li>
         </ul>
         <small>
           Copyright © 2019 htmlgo.site All rights reserved.
@@ -21,10 +22,48 @@
 
 <script>
 import './assets/css/reset.css'
-
+const url = 'https://badminton.htmlgo.site/'
 export default {
   name: 'app',
-  components: {},
+  data: function() {
+    return {
+      twitterLink: '',
+      facebookLink: '',
+    }
+  },
+  methods: {
+    makeSnsUrl(baseUrl, params) {
+      const paramArray = []
+      for (var key in params) {
+        paramArray.push(key + '=' + encodeURIComponent(params[key]))
+      }
+      return baseUrl + paramArray.join('&')
+    },
+    makeTwitterLink() {
+      const baseUrl = 'https://twitter.com/intent/tweet?'
+      const params = {
+        text:
+          document.title +
+          ' | ' +
+          document.getElementsByName('description').item(0).content,
+        url: url,
+      }
+      this.twitterLink = this.makeSnsUrl(baseUrl, params)
+    },
+    makeFacebookLink() {
+      const baseUrl = 'https://www.facebook.com/dialog/share?'
+      const params = {
+        app_id: document.querySelector('meta[property="fb:app_id"]').content,
+        display: 'popup',
+        href: url,
+      }
+      this.facebookLink = this.makeSnsUrl(baseUrl, params)
+    },
+  },
+  created() {
+    this.makeTwitterLink()
+    this.makeFacebookLink()
+  },
 }
 </script>
 
